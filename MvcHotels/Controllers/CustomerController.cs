b8f6cad1670aa2db56c9d47922b5ea4538ee4547
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-11-03 11:40:50 +0300

    Add project files.

---
+++ MvcHotels/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHotels.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
